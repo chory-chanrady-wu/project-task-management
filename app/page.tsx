baef8a460b9620602613a6e22d0be69fdae8286c
CHORY-Chanrady <chorychanrady168@gmail.com>
2026-01-08 23:01:52 +0700

    full project commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { fetchProjects, fetchTasks } from "@/lib/api";
-import { Bell, BellRing, Plus } from "lucide-react";
+import { Bell, Plus } from "lucide-react";
 import Link from "next/link";
 import RecentlyTasks from "./components/recentlytasks";
 import RecentlyProjects from "./components/recentlyprojects";
@@ -14,25 +14,6 @@ export default async function DashboardPage() {
   const projects = await fetchProjects();
   return (
     <main className="flex-1 bg-gray-100 min-h-screen">
-      {/* <div className="mb-4 bg-sidebar flex items-center justify-between shadow z-10 top-0 px-10">
-        <div>
-          <h1 className="text-2xl font-bold m-2">Dashboard</h1>
-          <p className="text-gray-600 m-2">
-            Welcome back! Here's an overview of your tasks.
-          </p>
-        </div>
-        <div className="space-x-4 flex items-center">
-          <span>
-            <BellRing className="hover:scale-106" />
-          </span>
-          <Link href="/tasks/new">
-            <button className="bg-black text-white px-4 py-2 shadow rounded-md hover:bg-gray-700 hover:scale-105 transition-transform">
-              <Plus className="inline mr-2" /> New Task
-            </button>
-          </Link>
-        </div>
-      </div> */}
-
       <header className="bg-white border-b border-gray-200 px-8 py-2 fixed top-0 left-64 right-0 z-50">
         <div className="flex items-center justify-between">
           <div>
